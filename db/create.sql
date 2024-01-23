52a59b9a9ee20023faa9936187a0fb99c646b814
Kiki05M <115073785+Kiki05M@users.noreply.github.com>
2024-01-23 16:42:12 +0000

    db products name, desc., code and price

--- db/create.sql
+++ db/create.sql
@@ -20,11 +20,20 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('anua heartleaf cleansing oil', 'An extraordinary oil cleanser enriched with the soothing essence of heartleaf extract to help tackle blemishes and promote a clearer, healthy-looking complexion. Formulated with a non-comedogenic blend of nourishing plant-based oils, including jojoba, olive fruit and grape seed, to effortlessly purify your skin and leave it revitalized.
-Its lightweight formula ensures that irritations are kept at bay, allowing your skin to breathe and feel at ease.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('moisturizer', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('toner', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('masks', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('sunscreen', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('skin boosters', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Anua heartleaf cleansing oil', 'An extraordinary oil cleanser enriched with the soothing essence of heartleaf extract to help tackle blemishes and promote a clearer, healthy-looking complexion. Formulated with a non-comedogenic blend of nourishing plant-based oils, including jojoba, olive fruit and grape seed, to effortlessly purify your skin and leave it revitalized.
+Its lightweight formula ensures that irritations are kept at bay, allowing your skin to breathe and feel at ease.', '816905633-0', 24.99);
+insert into products (name, description, code, price) values ('COSRX advanced snail 92 all in one moisturizer', 'A lightweight cream enriched with 92% snail secretion filtrate to drench your skin with its needed moisture, helping to repair damage and promote healing without feeling sticky.
+  Increased moisture diminishes fine lines and wrinkles, resulting in a healthier, younger look.', '177030122-3', 28.99);
+insert into products (name, description, code, price) values ('Anua heartleaf 77% soothing toner', 'A light and hydrating toner especially developed for sensitive skin.
+  Formulated with 77% Heartleaf extract to soothe and calm redness while protecting your skin against external stressors.
+  Contains houttuynia cordata extract which is known for its moisturizing, anti-bacterial and anti-inflammatory properties.', '845924201-X', 24.99);
+insert into products (name, description, code, price) values ('Round Lab 1025 dokdo water gel sheet mask', 'A luxury gel sheet mask featuring triple hyaluronic acid, deep sea water and coconut water which wrap around your skin and deeply nourish it.
+  Becomes watery and transparent like a gel, instantly cooling and soothing your skin.
+  Suitable for all skin types.', '393155505-7', 3.50);
+insert into products (name, description, code, price) values ('Beauty of Joseon relief sunscreen', 'An organic sunscreen that comes in a lightweight and quick-absorbent texture to allow for fresh absorption without pilling or leaving any white cast.
+  Formulated with rice extract and grain-derived probiotics to help nourish natural skin strength.
+  Free of oxybenzone, octinoxate and animal-derived ingredients, making it suitable for sensitive skin.
+  SPF 50+ PA++++', '586928463-6', 18,99);
+insert into products (name, description, code, price) values ('COSRX advanced snail 96 mucin power essence', 'An elasticity enhancing essence infused with hypoallergenic snail secretion filtrate to help soothe your skin.
+  Enriched with various nutritious ingredients to improve damaged skin and keep it moisturized all day.', '192662523-7', 23.99);
 
